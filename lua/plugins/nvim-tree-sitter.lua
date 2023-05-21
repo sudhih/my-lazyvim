6c7d9614a5526e21f46dd3024da6e8a42512b1cb
Sudhir Hanumanthappa <sudhih@gmail.com>
2023-05-21 18:30:13 +0530

    - updated lazyvim's default nvim-treesitter's default parsers - renamed file 'p-zk-nvim' to 'zk-nvim'

---
+++ lua/plugins/nvim-tree-sitter.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "javascript",
+      "json",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+  },
+}
