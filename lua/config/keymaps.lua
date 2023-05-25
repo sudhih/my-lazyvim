9702291719522601d739a5352293c6d24189659d
Sudhir Hanumanthappa <sudhih@gmail.com>
2023-05-25 09:37:47 +0530

    key mapping for ESC, save, save-exit, quit-all, save-all-exit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set({ "n", "v", "i" }, "fj", "<ESC>", { noremap = true, desc = "Quick ESC" })
+vim.keymap.set({ "n", "v", "i" }, ",e", "<ESC>")
+vim.keymap.set({ "n", "v", "i" }, ",w", "<ESC>:w<CR>")
+vim.keymap.set({ "n", "v", "i" }, ",x", "<ESC>:x<CR>")
+vim.keymap.set({ "n", "v", "i" }, ",xx", "<ESC>:xa<CR>")
+vim.keymap.set({ "n", "v", "i" }, ",q", "<ESC>:q<CR>")
+vim.keymap.set({ "n", "v", "i" }, ",qq", "<ESC>:qa<CR>")
