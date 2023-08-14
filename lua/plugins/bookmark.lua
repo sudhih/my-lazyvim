8ce23c396374d544550bb20fb0d5326637970551
Sudhir Hanumanthappa <sudhih@gmail.com>
2023-08-14 19:06:58 +0530

    install vim-bookmark plugin

---
+++ lua/plugins/bookmark.lua
@@ -0,0 +1,13 @@
+return {
+
+  {
+    "MattesGroeger/vim-bookmarks",
+    config = function()
+      vim.g.bookmark_save_per_working_dir = 1
+      vim.g.bookmark_auto_save = 1
+    end,
+  },
+
+  --   "tom-anders/telescope-vim-bookmarks.nvim",
+  -- },
+}
