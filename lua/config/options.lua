f7a568bb551d7a0a88085644cceef55c0d256465
Sudhir Hanumanthappa <sudhih@gmail.com>
2023-09-03 20:55:55 +0530

    - changed default leader key from space to ',' - few tokyonight.nvim, the colorschemes configs such as cursorline   highlight - disabled mini.pair plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.mapleader = ","
+vim.g.maplocalleader = " "
 -- wrap lines to the screen width or window width
 vim.opt.wrap = true
 if vim.fn.has("WSL") == 1 then
---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      -- the themes comes in 3 styles `storm`, `moom` & a darker variant `day` and `night`
+      style = "storm",
+      on_highlights = function(hl, c)
+        hl.CursorLineNr = { fg = c.orange, bold = true }
+      end,
+      -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      day_brightness = 0.2,
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
+    },
+  },
+}
