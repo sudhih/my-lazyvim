7365ccb0ffc332f4e906c5467d121eb3f22a63a5
Sudhir Hanumanthappa <sudhih@gmail.com>
2023-09-15 21:54:41 +0530

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,13 +3,14 @@
 -- Add any additional keymaps here
 
 local set_kmap = vim.keymap.set
+local nvi = { "n", "v", "i" } -- modes
 
-set_kmap({ "n", "v", "i" }, "fj", "<ESC>", { noremap = true, desc = "Quick ESC" })
-set_kmap({ "n", "v", "i" }, "<leader>e", "<ESC>")
-set_kmap({ "n", "v", "i" }, "<leader>w", "<ESC>:w<CR>")
-set_kmap({ "n", "v", "i" }, "<leader>x", "<ESC>:x<CR>")
-set_kmap({ "n", "v", "i" }, "<leader>xx", "<ESC>:xa<CR>", { desc = "Save All & Exit" })
-set_kmap({ "n", "v", "i" }, "<leader>q", "<ESC>:q<CR>")
-set_kmap({ "n", "v", "i" }, "<leader>qq", "<ESC>:qa<CR>", { desc = "Quit All" })
+set_kmap(nvi, "fj", "<ESC>", { noremap = true, desc = "Quick ESC" })
+set_kmap(nvi, "<leader>e", "<ESC>")
+set_kmap(nvi, "<leader>w", "<ESC>:w<CR>")
+set_kmap(nvi, "<leader>x", "<ESC>:x<CR>")
+set_kmap(nvi, "<leader>xx", "<ESC>:xa<CR>", { desc = "Save All & Exit" })
+set_kmap(nvi, "<leader>q", "<ESC>:q<CR>")
+set_kmap(nvi, "<leader>qq", "<ESC>:qa<CR>", { desc = "Quit All" })
 
 set_kmap("n", "<C-f>", ":!tmux-sessionizer<CR>", { noremap = true, desc = "Open `tmux-sessionizer`" })
