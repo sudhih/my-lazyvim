f9d772d7d030237cba4250ad2f9caab70b12ddeb
Sudhir Hanumanthappa <sudhih@gmail.com>
2024-01-28 01:01:54 +0530

    udpate
    new implementation for keymapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,15 +2,28 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local set_kmap = vim.keymap.set
-local nvi = { "n", "v", "i" } -- modes
+local kmap = function(lhs, rhs, description)
+  local modes = { "n", "v", "i" }
+  local opts = { noremap = true, desc = description }
+  vim.keymap.set(modes, lhs, rhs, opts)
+end
 
-set_kmap(nvi, "fj", "<ESC>", { noremap = true, desc = "Quick ESC" })
-set_kmap(nvi, "<leader>e", "<ESC>")
-set_kmap(nvi, "<leader>w", "<ESC>:w<CR>")
-set_kmap(nvi, "<leader>x", "<ESC>:x<CR>")
-set_kmap(nvi, "<leader>xx", "<ESC>:xa<CR>", { desc = "Save All & Exit" })
-set_kmap(nvi, "<leader>q", "<ESC>:q<CR>")
-set_kmap(nvi, "<leader>qq", "<ESC>:qa<CR>", { desc = "Quit All" })
+kmap("<leader>e", "<ESC>")
+kmap("fj", "<ESC>")
+kmap("<leader>w", "<ESC>:w<RETURN>", "Save")
+kmap("<leader>xx", "<ESC>:xa<RETURN>", "Save all & Exit")
+kmap("<leader>q", "<ESC>:q<RETURN>", "Quit Window")
+kmap("<leader>qq", "<ESC>:qa<RETURN>", "Quit All Window")
 
-set_kmap("n", "<C-f>", ":!tmux-sessionizer<CR>", { noremap = true, desc = "Open `tmux-sessionizer`" })
+-- local set_kmap = vim.keymap.set
+-- local nvi = { "n", "v", "i" } -- modes
+--
+-- -- set_kmap(nvi, "fj", "<ESC>", { noremap = true, desc = "Quick ESC" })
+-- set_kmap(nvi, "<leader>e", "<ESC>")
+-- set_kmap(nvi, "<leader>w", "<ESC>:w<CR>")
+-- set_kmap(nvi, "<leader>x", "<ESC>:x<CR>")
+-- set_kmap(nvi, "<leader>xx", "<ESC>:xa<CR>", { desc = "Save All & Exit" })
+-- set_kmap(nvi, "<leader>q", "<ESC>:q<CR>")
+-- set_kmap(nvi, "<leader>qq", "<ESC>:qa<CR>", { desc = "Quit All" })
+--
+-- set_kmap("n", "<C-f>", ":!tmux-sessionizer<CR>", { noremap = true, desc = "Open `tmux-sessionizer`" })
